acf8c467f091da64ea5f48f9d09640c10d2082d0
alphapo05 <alphapo05@gmail.com>
2024-09-14 21:52:05 +0900

    [Test] 포물선 속도값 일정하게 보정하는 기능 추가와 카메라 무빙, 임시 에임 추가

--- Assets/Scripts/BoomParabola.cs
+++ Assets/Scripts/BoomParabola.cs
@@ -1,56 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoomParabola : MonoBehaviour
 {
-    public GameObject Ball;         //던질 임시 오브젝트
+    public bool ready = true;       //임시로 작동 테스트
 
-    public bool Ready = true;
+    public Transform FirePosition;
 
-    public double velocity = 3f;
+    public Vector3 startPosition;
+    public Vector3 targetPosition;
+
+    private Vector3 targetDistance;
+
+    public float velocity = 30f;
     public float gravity = 9.82f;
+    public float arriveTime = 3f;
 
-    private float XPos = 0f;
-    private float YPos = 0f;
-    private float ZPos = 0;
-
-    private float fx = 0;
-    private float fy = 0;
-    private float fz = 0;
+    private Vector3 currentPosition;
+    private Vector3 initialVelocity;
 
     private float currentTime = 0;
+
+    private MeshRenderer meshRenderer;
+
+    private Ray ray;
+    private RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetMouseButtonDown(0))
         {
-            Ready = false;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log("발사");
+            if (Physics.Raycast(ray, out hit, 30f))
+            {
+                targetPosition = hit.point;
+                startPosition = FirePosition.position;
+                float dis = Vector3.Distance(startPosition, targetPosition);
+                arriveTime = dis / velocity;
+                currentTime = 0;
+                transform.position = startPosition;
+                CalculateVelocity();
+                ready = false;
+                meshRenderer.enabled = true;
+            }
         }
 
-        if (Ready) return;
+        if (ready) return;
 
-        ThrowBoom(Ball);
+        ThrowBoom();
         
     }
 
-    private void ThrowBoom(GameObject boom)
+    private void CalculateVelocity()
     {
-        CurrentPosition(boom.transform.position);
+        targetDistance = targetPosition - startPosition;
+        float x = targetDistance.x;
+        float y = targetDistance.y;
+        float z = targetDistance.z;
+
+        initialVelocity.x = x / arriveTime;
+        initialVelocity.z = z / arriveTime;
+
+        //실제 이동 거리 = 기존 속도로 이동한 거리 - 중력가속도로 이동한 거리
+        //기존 속도로 이동한 거리 = 실제 이동거리(y) + 중력가속도로 이동한 거리
+        initialVelocity.y = (y + 0.5f * gravity * arriveTime * arriveTime) / arriveTime;
     }
 
-    private void CurrentPosition(Vector3 boom)
+    private void ThrowBoom()
     {
-        XPos = boom.x;
-        YPos = boom.y;
-        ZPos = boom.z;
+        
+        if (currentTime < arriveTime)
+        {
+            currentTime += Time.deltaTime;
+            currentPosition = startPosition + initialVelocity * currentTime;
+            currentPosition.y -= 0.5f * gravity * currentTime * currentTime;
+
+            transform.position = currentPosition;
+        }
+        else
+        {
+            ready = true;
+            meshRenderer.enabled = false;
+            Debug.Log("도착" + transform.position);
+        }
     }
+
 
 }
